7cd65630a7d8bab888e3c6b034eba6148faaaa83
Juan Rodriguez <icalcifer@Juans-MacBook-Pro.local>
2017-10-03 19:09:38 -0700

    Initial Commit

---
+++ UrgenC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UrgenC
+//
+//  Created by Juan Rodriguez on 10/3/17.
+//  Copyright © 2017 Juan Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
